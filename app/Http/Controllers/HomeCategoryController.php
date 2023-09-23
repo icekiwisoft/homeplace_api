2950a5d3535729e01a5a02ba92bb62599125479e
ngdream <96875506+ngdream@users.noreply.github.com>
2023-09-23 08:57:46 +0100

    first commit

---
+++ app/Http/Controllers/HomeCategoryController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\HomeCategory;
+use Illuminate\Http\Request;
+
+class HomeCategory extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(HomeCategory $homeCategory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(HomeCategory $homeCategory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, HomeCategory $homeCategory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(HomeCategory $homeCategory)
+    {
+        //
+    }
+}
---
+++ app/Models/Announcer.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Announcer extends Model
+{
+    use HasFactory;
+}
