4aa20673be122e35739253b45123a6d014a71d5a
ngdream <96875506+ngdream@users.noreply.github.com>
2023-10-11 00:18:43 +0100

    Update MediaController.php

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -18,10 +18,10 @@ class MediaController extends Controller
             $path = storage_path('app/upload/' . $fichier->file);
             if (file_exists($path)) {
                 return response()->file($path);
-            }else{
+            } else {
                 return response()->json(['message' => 'fichier pas vu'], 404);
             };
-        }else{
+        } else {
             return response()->json(['message' => 'fichier not found.'], 404);
         };
     }
@@ -30,39 +30,39 @@ class MediaController extends Controller
      * Show the form for creating a new resource.
      */
     public function create(Request $request)
-        {
-            try{
-                $fichier = $request->file('file');
-                // Supprimer les espaces dans le nom de fichier
-                $filename = str_replace(' ', '', $fichier->getClientOriginalName());
-                
-                $extension = $fichier->getClientOriginalExtension();
-                // Récupérer la taille du fichier en bytes
-                $size = $fichier->getSize();
-                if($size >= 1000000){
-                    return response()->json([
-                        'status_code' => 422,
-                        'status_message' => 'la taille du fichier doit etre inferieure a 1 Mo',
-                    ]);
-                }else{
-                    $fichier_save = $request->file->storeAs('upload', $filename);
-                    $img = new Media();
-                    $img->file = $fichier->getClientOriginalName();
-                    $img->description = $request->description;
-                    $img->save();
-                    
+    {
+        try {
+            $fichier = $request->file('file');
+            // Supprimer les espaces dans le nom de fichier
+            $filename = str_replace(' ', '', $fichier->getClientOriginalName());
+
+            $extension = $fichier->getClientOriginalExtension();
+            // Récupérer la taille du fichier en bytes
+            $size = $fichier->getSize();
+            if ($size >= 1000000) {
+                return response()->json([
+                    'status_code' => 422,
+                    'status_message' => 'la taille du fichier doit etre inferieure a 1 Mo',
+                ]);
+            } else {
+                $fichier_save = $request->file->storeAs('upload', $filename);
+                $img = new Media();
+                $img->file = $fichier->getClientOriginalName();
+                $img->description = $request->description;
+                $img->save();
+
                 return response()->json([
                     'status_code' => 200,
                     'status_message' => 'fichier ajouter avec succes',
                     'data' => $img
                 ]);
-                };
-            }catch(Exception $e){
-                return response()->json($e);
-            }
+            };
+        } catch (Exception $e) {
+            return response()->json($e);
         }
+    }
 
-   // // /**
+    // // /**
     //  * Store a newly created resource in storage.
     //  */
     // public function store(Request $request)
@@ -85,45 +85,45 @@ class MediaController extends Controller
     // {
     //     //
     // }
-//
+    //
     /**
      * Update the specified resource in storage.
      */
     public function update(Request $request, Media $media)
     {
-        try{
+        try {
             $media->file = $request->input('file');
-                $media->description = $request->description;
-                $filename = str_replace(' ', '', $media->getClientOriginalName());
-                if($media->file !=  null && $media->description!=null){
-                    $size = $media->getSize();
-                    if($size >= 1000000){
-                        return response()->json([
-                            'status_code' => 422,
-                            'status_message' => 'la taille du fichier doit etre inferieure a 1 Mo',
-                        ]);
-                    }else{
-                        $fichier_save = $request->file->storeAs('upload', $filename);
-                        // $img = new Media();
-                        $media->file = $request->getClientOriginalName();
-                        $media->description = $request->description;
-                        $media->save();
-                        
+            $media->description = $request->description;
+            $filename = str_replace(' ', '', $media->getClientOriginalName());
+            if ($media->file !=  null && $media->description != null) {
+                $size = $media->getSize();
+                if ($size >= 1000000) {
+                    return response()->json([
+                        'status_code' => 422,
+                        'status_message' => 'la taille du fichier doit etre inferieure a 1 Mo',
+                    ]);
+                } else {
+                    $fichier_save = $request->file->storeAs('upload', $filename);
+                    // $img = new Media();
+                    $media->file = $request->getClientOriginalName();
+                    $media->description = $request->description;
+                    $media->save();
+
                     return response()->json([
                         'status_code' => 200,
                         'status_message' => 'fichier modifier avec succes',
                         'data' => $img
                     ]);
-                    };
-                }else{
-                    return response()->json([
-                        'status_code' => 422,
-                        'status_message' => 'veuillez remplir les champs',
-                    ]);
                 };
-        }catch(Exception $e){
-                return response()->json($e);
-           }
+            } else {
+                return response()->json([
+                    'status_code' => 422,
+                    'status_message' => 'veuillez remplir les champs',
+                ]);
+            };
+        } catch (Exception $e) {
+            return response()->json($e);
+        }
     }
 
     /**
@@ -131,11 +131,11 @@ class MediaController extends Controller
      */
     public function delete(Media $media)
     {
-         try{
+        try {
             // $pos = post::find($request->post_id);
-            if( $media){
+            if ($media) {
                 // if($pos->user_id == auth()->user()->id){
-                    $media->delete();
+                $media->delete();
                 // }else{
                 //     return response()->json([
                 //         'status_code' => 422,
@@ -147,15 +147,14 @@ class MediaController extends Controller
                     'status_message' => 'media supprimer avec succes',
                     'data' => $media
                 ]);
-            }else{
+            } else {
                 return response()->json([
                     'status_code' => 422,
                     'status_message' => 'media introuvable!'
                 ]);
             }
-        }catch(Ecxeption $e){
+        } catch (Ecxeption $e) {
             return response()->json($e);
         }
     }
-
 }
